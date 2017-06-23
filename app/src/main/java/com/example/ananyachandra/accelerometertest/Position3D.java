ab8be5b2b7531063ac43ddc2adb74fb2b0a70df0
Ananya Chandra <ananya.chandra@PW-C02PR06TG8WN.local>
2017-06-23 11:08:13 +0530

    Http request made, not working.

--- app/src/main/java/com/example/ananyachandra/accelerometertest/Position3D.java
+++ app/src/main/java/com/example/ananyachandra/accelerometertest/Position3D.java
@@ -1,7 +1,26 @@
 package com.example.ananyachandra.accelerometertest;
 
-import android.os.Handler;
+import android.os.AsyncTask;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -24,6 +43,7 @@ public class Position3D {
     Coordinate Y;
     Coordinate Z;
     int counter = 0;
+    int sendCounter = 0;
     boolean updateAllowed = false;
 
     final float ACC_THRESHOLD = 0.5f;
@@ -37,13 +57,19 @@ public class Position3D {
     public void run (float ax, float ay, float az, float t) {
 
         updateParameters(ax, t, X);
-//        updateParameters(ay, t, Y);
-//        updateParameters(az, t, Z);
+        updateParameters(ay, t, Y);
+        updateParameters(az, t, Z);
+
+        updateAllowed = false;
 
         if(counter == 10) {
             updateAllowed = true;
             counter = 0;
-//            System.out.println("X: " + X.getValue() * 100 + "\t\t Y: " + Y.getValue() * 100 + "\t\t Z: " + Z.getValue() * 100);
+            sendCounter++;
+            System.out.println("X: " + X.getValue() * 100 + "\t\t Y: " + Y.getValue() * 100 + "\t\t Z: " + Z.getValue() * 100);
+        }
+        if(sendCounter == 50) {
+            new RequestTask().execute("");
         }
         counter++;
     }
@@ -66,7 +92,7 @@ public class Position3D {
             thisIterationA = getGuassianAcceleration(C.getAccsOfLast5Iterations());
 
             if (!(fallInThreshold(thisIterationA, ACC_THRESHOLD))) {
-//                System.out.println("a: " + thisIterationA + "\t\t v: " + C.getCurrV() + "\t\t dir: " + direction + "\t\t follup: " + isFollowUp);
+//                System.out.println("a: " + thisIterationA + "\t\t v: " + C.getCurrV() + "\t\t dir: " + C.getDirection() + "\t\t follup: " + C.isFollowUp());
                 float s = 0;
 //                s = currV * t + 0.5f * thisIterationA * t * t;
 
@@ -87,14 +113,14 @@ public class Position3D {
                     if(!C.isFollowUp()) {
                         C.setDirection(-1);
 //                        System.out.println("a: " + thisIterationA + "\t\t -ve");
-//                        System.out.println("s: " + s + "\t\t -ve");
+//                        System.out    .println("s: " + s + "\t\t -ve");
                         s = C.getCurrV() * t;
 //                        s = currV * t + 0.5f * thisIterationA * t * t;
                     }
                 }
 
                 C.setValue(C.getValue() + s);
-                System.out.println("X: " + C.getValue() * 100);
+//                System.out.println("X: " + C.getValue() * 100);
 
                 C.setPrevV(C.getCurrV());
                 C.setCurrV(C.getCurrV() + thisIterationA*t);
@@ -167,5 +193,73 @@ public class Position3D {
         }
 
         return gaussian;
+    }
+
+    private void sendCoords() {
+//        HttpClient httpClient = new DefaultHttpClient();
+//        HttpPost httpPost = new HttpPost("https://172.20.164.13:1729/plot");
+//
+//        List<NameValuePair> nameValuePair = new ArrayList<NameValuePair>();
+//        nameValuePair.add(new BasicNameValuePair("Content-Type", "application/json"));
+//
+//        //Encoding POST data
+//        try {
+//            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePair));
+//        } catch (UnsupportedEncodingException e)
+//        {
+//            e.printStackTrace();
+//        }
+//
+//        // Making HTTP Request
+//        try {
+//            HttpResponse response = httpClient.execute(httpPost);
+//        } catch (ClientProtocolException e) {
+//            // writing exception to log
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            // writing exception to log
+//            e.printStackTrace();
+//        }
+
+        try{
+            URL url = new URL("https://172.20.164.13:1729/plot");
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestMethod("POST");
+            connection.setDoInput(true);
+
+            JSONObject coords = new JSONObject();
+            try {
+                coords.put("X", X.getValue());
+                coords.put("Y", Y.getValue());
+                coords.put("Z", Z.getValue());
+
+                OutputStreamWriter wr= new OutputStreamWriter(connection.getOutputStream());
+                wr.write(coords.toString());
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+        catch (IOException e) {
+            // Writing exception to log
+            e.printStackTrace();
+        }
+    }
+
+    class RequestTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... uri) {
+            sendCoords();
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            System.out.println("Coords Sent");
+        }
     }
 }
